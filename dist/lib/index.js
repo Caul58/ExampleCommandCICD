c3004f91f806485b1a5977f4660c8774f63c5fe7
CARLOS RAUL PEREZ MORENO <caul58@gmail.com>
2019-11-07 02:38:07 +0100

    feat(): Jarvis command added

---
+++ dist/lib/index.js
@@ -0,0 +1,82 @@
+#!/usr/bin/env node
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const chalk = require('chalk');
+const path = require('path');
+const program = require('commander');
+const { exec } = require('child_process');
+program
+    .version('0.0.1')
+    .description("An example CLI for construct flutter apps");
+program
+    .command('stage <stageName>')
+    .description('run the required stage')
+    .action(function (stageName) {
+    switch (stageName) {
+        case "analyze": {
+            console.log("Analyze");
+            console.log("mierda");
+            exec("/Users/caulpermor/Development/flutter/bin/flutter analyze --no-pub", (err, stdout, stderr) => {
+                if (err) {
+                    console.log(stderr);
+                }
+                else {
+                    console.log(stdout);
+                }
+            });
+            break;
+        }
+        case "build": {
+            console.log("Build");
+            exec("/Users/caulpermor/Development/flutter/bin/flutter build ios --release --no-codesign", (err, stdout, stderr) => {
+                if (err) {
+                    console.log(stderr);
+                }
+                else {
+                    console.log(stdout);
+                }
+            });
+            break;
+        }
+        case "test": {
+            console.log("Test");
+            exec("/Users/caulpermor/Development/flutter/bin/flutter test --coverage", (err, stdout, stderr) => {
+                if (err) {
+                    console.log(stderr);
+                }
+                else {
+                    console.log(stdout);
+                }
+            });
+            break;
+        }
+        case "sign": {
+            console.log("Sign");
+            exec("cd ios && /usr/bin/xcodebuild -list", (err, stdout, stderr) => {
+                if (err) {
+                    console.log(stderr);
+                }
+                else {
+                    console.log(stdout);
+                }
+            });
+            break;
+        }
+        default: {
+            console.log("No valid option selected");
+            break;
+        }
+    }
+}).on('--help', function () {
+    console.log('');
+    console.log('Examples:');
+    console.log('');
+    console.log('  $ jarvis stage analyze');
+    console.log('  $ jarvis stage build');
+    console.log('  $ jarvis stage test');
+    console.log('  $ jarvis stage sign');
+});
+program.parse(process.argv);
+if (!process.argv.slice(2).length) {
+    program.outputHelp();
+}
---
+++ lib/index.ts
@@ -0,0 +1,78 @@
+#!/usr/bin/env node
+
+const program = require('commander');
+const { exec } = require('child_process');
+
+program
+    .version('0.0.1')
+    .description("An example CLI for construct flutter apps");
+
+program
+    .command('stage <stageName>')
+    .description('run the required stage')
+    .action(function(stageName: String){
+        switch (stageName) {
+            case "analyze": {
+                console.log("Analyze");
+                exec("/Users/caulpermor/Development/flutter/bin/flutter analyze --no-pub", (err: any, stdout: any, stderr: any) => {
+                    if (err){
+                        console.log(stderr);
+                    } else {
+                        console.log(stdout);
+                    }
+                });
+                break;
+            }
+            case "build":{
+                console.log("Build");
+                exec("/Users/caulpermor/Development/flutter/bin/flutter build ios --release --no-codesign", (err: any, stdout: any, stderr: any) => {
+                    if (err){
+                        console.log(stderr);
+                    } else {
+                        console.log(stdout);
+                    }
+                });
+                break;
+            }
+            case "test": {
+                console.log("Test");
+                exec("/Users/caulpermor/Development/flutter/bin/flutter test --coverage", (err: any, stdout: any, stderr: any) => {
+                    if (err){
+                        console.log(stderr);
+                    } else {
+                        console.log(stdout);
+                    }
+                });
+                break;
+            }
+            case "sign": {
+                console.log("Sign");
+                exec("cd ios && /usr/bin/xcodebuild -list", (err: any, stdout: any, stderr: any) => {
+                    if (err){
+                        console.log(stderr);
+                    } else {
+                        console.log(stdout);
+                    }
+                });
+                break;
+            }
+            default: {
+                console.log("No valid option selected");
+                break;
+            }
+        }
+    }).on('--help', function(){
+        console.log('');
+        console.log('Examples:');
+        console.log('');
+        console.log('  $ jarvis stage analyze');
+        console.log('  $ jarvis stage build');
+        console.log('  $ jarvis stage test');
+        console.log('  $ jarvis stage sign');
+    })
+
+program.parse(process.argv);
+
+if (!process.argv.slice(2).length) {
+	program.outputHelp();
+}
